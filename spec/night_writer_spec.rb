ec260d012476ea76479827a525af22b3d050d4f9
Andrew Speth <speth.andrew@yahoo.com>
2022-03-06 14:03:19 -0700

    test: can translate a single letter

--- spec/night_writer_spec.rb
+++ spec/night_writer_spec.rb
@@ -1,25 +1,36 @@
 require './lib/night_writer'
 
 RSpec.describe NightWriter do
-  before(:each) do
-    ARGV[0] = 'fake_message.txt'
-    ARGV[1] = 'fake_braille.txt'
-    @night_writer = NightWriter.new
+  context "using fake files" do
+    before(:each) do
+      ARGV[0] = 'fake_message.txt'
+      ARGV[1] = 'fake_braille.txt'
+      @night_writer = NightWriter.new
+    end
+
+    it "exists" do
+      expect(@night_writer).to be_a(NightWriter)
+    end
+
+    it "can access incoming message" do
+      expect(@night_writer.incoming_text.chomp).to eq('hello world')
+    end
+
+    it "can print the number of characters in first input file" do
+      expect(@night_writer.incoming_character_count).to eq(12)
+    end
   end
 
-  it "exists" do
-    expect(@night_writer).to be_a(NightWriter)
-  end
+  context "using real files" do
+    before(:each) do
+      ARGV[0] = 'message.txt'
+      ARGV[1] = 'braille.txt'
+      @night_writer = NightWriter.new
+    end
 
-  it "can access incoming message" do
-    expect(@night_writer.incoming_text.chomp).to eq('hello world')
-  end
-
-  it "can print the number of characters in first input file" do
-    expect(@night_writer.character_count).to eq(12)
-  end
-
-  it "can move text to a new file" do
-    expect(@night_writer.outgoing_text).to be_a(File)
+    it "can translate a single letter" do
+      @night_writer.translate
+      expect(@night_writer.translated_text).to eq("O. \n.. \n.. ")
+    end
   end
 end
