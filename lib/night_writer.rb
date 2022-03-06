e05a6b36b1b0ff05638373bb9b326d19d1828b40
Andrew Speth <speth.andrew@yahoo.com>
2022-03-06 09:41:56 -0700

    feat: can move text to a new file

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,11 +1,16 @@
 class NightWriter
-  attr_reader :incoming_text
+  attr_reader :incoming_text, :outgoing_text
 
   def initialize
     @incoming_text = File.new(ARGV[0], "r")
+    @outgoing_text = File.new(ARGV[1], "w")
   end
 
   def character_count
     @incoming_text.read.length
+  end
+
+  def move_text
+    @outgoing_text.write(@incoming_text)
   end
 end
