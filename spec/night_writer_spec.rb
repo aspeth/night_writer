2d3ba2f7024491ecdbb3e5bbb89c9a6e56cc3a6c
Andrew Speth <speth.andrew@yahoo.com>
2022-03-06 09:13:09 -0700

    test: NightWriter exists

---
+++ spec/night_writer_spec.rb
@@ -0,0 +1,11 @@
+require './lib/night_writer'
+
+RSpec.describe NightWriter do
+  before(:each) do
+    @night_writer = NightWriter.new
+  end
+
+  it "exists" do
+    expect(@night_writer).to be_a(NightWriter)
+  end
+end
