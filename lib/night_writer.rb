ba468e0f3a598a430bb29194fdf9a414b6d276c4
Andrew Speth <speth.andrew@yahoo.com>
2022-03-06 09:28:45 -0700

    feat: can access incoming message

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,4 +1,7 @@
 class NightWriter
+  attr_reader :incoming_text
+
   def initialize
+    @incoming_text = File.new(ARGV[0], "r")
   end
 end
--- spec/night_writer_spec.rb
+++ spec/night_writer_spec.rb
@@ -2,6 +2,7 @@ require './lib/night_writer'
 
 RSpec.describe NightWriter do
   before(:each) do
+    ARGV[0] = 'fake_message.txt'
     @night_writer = NightWriter.new
   end
 
@@ -10,7 +11,6 @@ RSpec.describe NightWriter do
   end
 
   it "can access incoming message" do
-    ARGV[0] = 'fake_message.txt'
     expect(@night_writer.incoming_text).to be_a(File)
   end
 end
