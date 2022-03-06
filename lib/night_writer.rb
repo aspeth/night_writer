80d4f40a0a1886b57777655ad921be49533db0eb
Andrew Speth <speth.andrew@yahoo.com>
2022-03-06 14:03:47 -0700

    feat: can translate a single letter

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,16 +1,27 @@
+require './lib/dictionary'
+
 class NightWriter
-  attr_reader :incoming_text, :outgoing_text
+  include Dictionary
+  attr_reader :incoming_text
 
   def initialize
-    @incoming_text = File.new(ARGV[0], "r").read
-    @outgoing_text = File.new(ARGV[1], "w")
+    @incoming_text = File.open(ARGV[0], "r").read
+    @outgoing_file = File.open(ARGV[1], "w+")
   end
 
-  def character_count
+  def outgoing_text
+    @outgoing_file.read
+  end
+
+  def incoming_character_count
     @incoming_text.length
   end
 
-  def move_text
-    @outgoing_text.write(@incoming_text)
+  def translate
+    @outgoing_file.write(translated_text)
+  end
+
+  def translated_text
+    translated_text = english_to_braille(@incoming_text.chomp)
   end
 end
--- lib/night_writer_runner.rb
+++ lib/night_writer_runner.rb
@@ -1,5 +1,5 @@
 require './lib/night_writer'
 
 night_writer = NightWriter.new
-night_writer.move_text
-puts "Created '#{ARGV[1]}' containing #{night_writer.character_count} characters"
+night_writer.translate
+puts "Created '#{ARGV[1]}' containing #{night_writer.incoming_character_count} characters"
