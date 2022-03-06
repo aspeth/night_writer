a11e176c856f25ff1ab6154dc8b5f66f5cfb57f8
Andrew Speth <speth.andrew@yahoo.com>
2022-03-06 09:25:54 -0700

    test: can access incoming message

--- spec/night_writer_spec.rb
+++ spec/night_writer_spec.rb
@@ -8,4 +8,9 @@ RSpec.describe NightWriter do
   it "exists" do
     expect(@night_writer).to be_a(NightWriter)
   end
+
+  it "can access incoming message" do
+    ARGV[0] = 'fake_message.txt'
+    expect(@night_writer.incoming_text).to be_a(File)
+  end
 end
