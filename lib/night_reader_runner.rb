1f9ba845d56672b3c76794003ce27839330e2f64
Andrew Speth <speth.andrew@yahoo.com>
2022-03-07 15:48:47 -0700

    setup remainder of tests for NightReader

--- lib/night_reader_runner.rb
+++ lib/night_reader_runner.rb
@@ -1,4 +1,5 @@
 require './lib/night_reader'
 
 night_reader = NightReader.new
+night_reader.translate
 puts "Created '#{ARGV[1]}' containing #{night_reader.incoming_character_count} characters"
--- spec/night_reader_spec.rb
+++ spec/night_reader_spec.rb
@@ -20,4 +20,20 @@ RSpec.describe NightReader do
       expect(@night_reader.incoming_character_count).to eq(12)
     end
   end
+
+  context "using real files" do
+    it "can translate a single letter" do
+      ARGV[0] = 'braille_a.txt'
+      ARGV[1] = 'english_a.txt'
+      night_reader = NightReader.new
+      expect(night_reader.translated_text).to eq("a")
+    end
+
+    it "can read translated text" do
+      ARGV[0] = 'braille_abc.txt'
+      ARGV[1] = 'english_abc.txt'
+      night_reader = NightReader.new
+      expect(night_reader.translate).to eq(10)
+    end
+  end
 end
