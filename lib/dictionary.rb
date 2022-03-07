83e8f6c5241a3b8b5e0d9a96fb186a408c88f2d1
Andrew Speth <speth.andrew@yahoo.com>
2022-03-07 15:47:53 -0700

    fix: iteration to match new form of letter hash

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -1,112 +1,18 @@
-require './lib/night_writer'
+require './lib/alphabet'
 
 module Dictionary
   def english_to_braille(incoming_text)
-         letter_hash = {"a" => { 1 => "O.",
-                                 2 => "..",
-                                 3 => ".." },
-                        "b" => { 1 => "O.",
-                                 2 => "O.",
-                                 3 => ".." },
-                        "c" => { 1 => "OO",
-                                 2 => "..",
-                                 3 => ".." },
-                        "d" => { 1 => "OO",
-                                 2 => ".O",
-                                 3 => ".." },
-                        "e" => { 1 => "O.",
-                                 2 => ".O",
-                                 3 => ".." },
-                        "f" => { 1 => "OO",
-                                 2 => "O.",
-                                 3 => ".." },
-                        "g" => { 1 => "OO",
-                                 2 => "OO",
-                                 3 => ".." },
-                        "h" => { 1 => "O.",
-                                 2 => "OO",
-                                 3 => ".." },
-                        "i" => { 1 => ".O",
-                                 2 => "O.",
-                                 3 => ".." },
-                        "j" => { 1 => ".O",
-                                 2 => "OO",
-                                 3 => ".." },
-                        "k" => { 1 => "O.",
-                                 2 => "..",
-                                 3 => "O." },
-                        "l" => { 1 => "O.",
-                                 2 => "O.",
-                                 3 => "O." },
-                        "m" => { 1 => "OO",
-                                 2 => "..",
-                                 3 => "O." },
-                        "n" => { 1 => "OO",
-                                 2 => ".O",
-                                 3 => "O." },
-                        "o" => { 1 => "O.",
-                                 2 => ".O",
-                                 3 => "O." },
-                        "p" => { 1 => "OO",
-                                 2 => "O.",
-                                 3 => "O." },
-                        "q" => { 1 => "OO",
-                                 2 => "OO",
-                                 3 => "O." },
-                        "r" => { 1 => "O.",
-                                 2 => "OO",
-                                 3 => "O." },
-                        "s" => { 1 => ".O",
-                                 2 => "O.",
-                                 3 => "O." },
-                        "t" => { 1 => ".O",
-                                 2 => "OO",
-                                 3 => "O." },
-                        "u" => { 1 => "O.",
-                                 2 => "..",
-                                 3 => "OO" },
-                        "v" => { 1 => "O.",
-                                 2 => "O.",
-                                 3 => "OO" },
-                        "w" => { 1 => ".O",
-                                 2 => "OO",
-                                 3 => ".O" },
-                        "x" => { 1 => "OO",
-                                 2 => "..",
-                                 3 => "OO" },
-                        "y" => { 1 => "OO",
-                                 2 => ".O",
-                                 3 => "OO" },
-                        "z" => { 1 => "O.",
-                                 2 => ".O",
-                                 3 => "OO" },
-                        " " => { 1 => "..",
-                                 2 => "..",
-                                 3 => ".." },
-                  }
-    # top_row = []
-    # incoming_text.each_char { |char| top_row << letter_hash.values_at(char)[0][1] }
-    # middle_row = []
-    # incoming_text.each_char { |char| middle_row << letter_hash.values_at(char)[0][2] }
-    # bottom_row = []
-    # incoming_text.each_char { |char| bottom_row << letter_hash.values_at(char)[0][3] }
+
+    letter_hash = Alphabet.new.letter_hash
 
     rows = []
     formatted_rows = []
     3.times do |index|
       rows[index] = []
       formatted_rows[index] = []
-      incoming_text.each_char { |char| rows[index] << letter_hash.values_at(char)[0][index+1] }
+      incoming_text.each_char { |char| rows[index] << letter_hash.values_at(char)[0][index] }
       formatted_rows[index] = rows[index].each_slice(40).map { |string| string.join }
     end
-
-    # formatted_top_row = top_row.each_slice(40).map { |string| string.join }
-    # formatted_middle_row = middle_row.each_slice(40).map { |string| string.join }
-    # formatted_bottom_row = bottom_row.each_slice(40).map { |string| string.join }
-    #
-    # row1 = formatted_top_row
-    # row2 = formatted_middle_row
-    # row3 = formatted_bottom_row
 
     counter = formatted_rows[0].count
     index_num = 0
@@ -118,15 +24,8 @@ module Dictionary
     formatted_text
   end
 
-
-
-    # text_array = []
-    #
-    # new_text = ""
-    # incoming_text.each_char {|char| new_text.concat(letter_hash.values_at(char)[0][1]) }
-    # new_text.concat("\n")
-    # incoming_text.each_char {|char| new_text.concat(letter_hash.values_at(char)[0][2]) }
-    # new_text.concat("\n")
-    # incoming_text.each_char {|char| new_text.concat(letter_hash.values_at(char)[0][3]) }
-    # new_text
+  def braille_to_english(incoming_text)
+    letter_hash = Alphabet.new.letter_hash.invert
+    require "pry"; binding.pry
+  end
 end
