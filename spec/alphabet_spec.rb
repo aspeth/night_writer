7b19beaaecca8fb02568b4c2b63fa10d0c42deae
Andrew Speth <speth.andrew@yahoo.com>
2022-03-08 11:43:58 -0700

    test: Alphabet has attributes - passing

--- spec/alphabet_spec.rb
+++ spec/alphabet_spec.rb
@@ -5,4 +5,36 @@ RSpec.describe Alphabet do
     alpha = Alphabet.new
     expect(alpha).to be_a(Alphabet)
   end
+
+  it "has attributes" do
+    alpha = Alphabet.new
+    expect(alpha.letter_hash).to eq({"a" => ["O.", "..", ".."],
+                    "b" => ["O.", "O.", ".."],
+                    "c" => ["OO", "..", ".."],
+                    "d" => ["OO", ".O", ".."],
+                    "e" => ["O.", ".O", ".."],
+                    "f" => ["OO", "O.", ".."],
+                    "g" => ["OO", "OO", ".."],
+                    "h" => ["O.", "OO", ".."],
+                    "i" => [".O", "O.", ".."],
+                    "j" => [".O", "OO", ".."],
+                    "k" => ["O.", "..", "O."],
+                    "l" => ["O.", "O.", "O."],
+                    "m" => ["OO", "..", "O."],
+                    "n" => ["OO", ".O", "O."],
+                    "o" => ["O.", ".O", "O."],
+                    "p" => ["OO", "O.", "O."],
+                    "q" => ["OO", "OO", "O."],
+                    "r" => ["O.", "OO", "O."],
+                    "s" => [".O", "O.", "O."],
+                    "t" => [".O", "OO", "O."],
+                    "u" => ["O.", "..", "OO"],
+                    "v" => ["O.", "O.", "OO"],
+                    "w" => [".O", "OO", ".O"],
+                    "x" => ["OO", "..", "OO"],
+                    "y" => ["OO", ".O", "OO"],
+                    "z" => ["O.", ".O", "OO"],
+                    " " => ["..", "..", ".."],
+             })
+  end
 end
