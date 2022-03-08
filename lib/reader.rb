5b8c69509bc186d40f3b84279c8de87b4d80ace6
Andrew Speth <speth.andrew@yahoo.com>
2022-03-08 11:47:21 -0700

    formatting

--- lib/reader.rb
+++ lib/reader.rb
@@ -4,6 +4,7 @@ class Reader < NightObject
   def incoming_character_count
     (@incoming_text.delete("\n").length) / 6
   end
+  
   def translated_text
     translated_text = braille_to_english(@incoming_text.chomp)
   end
